9b25d018d8e702c8ba6d5d9acd600281700f8093
Bronson Avila <bronson.avila@gmail.com>
2018-07-05 09:43:58 -1000

    Refactor code blocks responsible for creating rainbow colors.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -8,72 +8,6 @@ function PixelPainter(width, height) {
 
   // ----------------------------------------------------------------------- //
 
-  function HSVtoRGB(h, s, v) {
-    // Source (Stack Overflow): https://goo.gl/8kW6EY
-    var r, g, b, i, f, p, q, t;
-    if (arguments.length === 1) {
-      (s = h.s), (v = h.v), (h = h.h);
-    }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-      case 0:
-        (r = v), (g = t), (b = p);
-        break;
-      case 1:
-        (r = q), (g = v), (b = p);
-        break;
-      case 2:
-        (r = p), (g = v), (b = t);
-        break;
-      case 3:
-        (r = p), (g = q), (b = v);
-        break;
-      case 4:
-        (r = t), (g = p), (b = v);
-        break;
-      case 5:
-        (r = v), (g = p), (b = q);
-        break;
-    }
-    return {
-      r: Math.round(r * 255),
-      g: Math.round(g * 255),
-      b: Math.round(b * 255)
-    };
-  }
-
-  function rainbow(p, length) {
-    // Source (Stack Overflow): https://goo.gl/8kW6EY
-    var rgb = HSVtoRGB((p / length) * 0.5, 1.0, 1.0);
-    return 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
-  }
-
-  function grayscale(num) {
-    return 'rgb(' + num + ',' + num + ',' + num + ')';
-  }
-
-  function changeHeading() {
-    headingColl[0].innerHTML =
-      '<span class="heading">P</span>' +
-      '<span class="heading">i</span>' +
-      '<span class="heading">x</span>' +
-      '<span class="heading">e</span>' +
-      '<span class="heading">l</span>' +
-      ' ' +
-      '<span class="heading">P</span>' +
-      '<span class="heading">a</span>' +
-      '<span class="heading">i</span>' +
-      '<span class="heading">n</span>' +
-      '<span class="heading">t</span>' +
-      '<span class="heading">e</span>' +
-      '<span class="heading">r</span>';
-    headingLetters = document.getElementsByClassName('heading');
-  }
-
   function buildGrid(width, height, id, rowClass, cellClass) {
     const grid = document.createElement('div');
     for (let i = 0; i < height; i++) {
@@ -95,6 +29,32 @@ function PixelPainter(width, height) {
     setTimeout(function() {
       btn.classList.toggle($class);
     }, 50);
+  }
+
+  function changeHeading() {
+    headingColl[0].innerHTML =
+      '<span class="heading">P</span>' +
+      '<span class="heading">i</span>' +
+      '<span class="heading">x</span>' +
+      '<span class="heading">e</span>' +
+      '<span class="heading">l</span>' +
+      ' ' +
+      '<span class="heading">P</span>' +
+      '<span class="heading">a</span>' +
+      '<span class="heading">i</span>' +
+      '<span class="heading">n</span>' +
+      '<span class="heading">t</span>' +
+      '<span class="heading">e</span>' +
+      '<span class="heading">r</span>';
+    headingLetters = document.getElementsByClassName('heading');
+  }
+
+  function makeColor(num) {
+    return 'hsl(' + num + ', 100%, 50%)';
+  }
+
+  function makeGrayscale(num) {
+    return 'rgb(' + num + ',' + num + ',' + num + ')';
   }
 
   // ----------------------------------------------------------------------- //
@@ -197,17 +157,18 @@ function PixelPainter(width, height) {
 
   // Set heading colors:
   changeHeading();
-  for (let i = 0; i < headingLetters.length; i++) {
-    headingLetters[i].style.color = rainbow(i, headingLetters.length / 1);
+  for (let i = 0, hue = 0; i < headingLetters.length; i++) {
+    headingLetters[i].style.color = makeColor(hue);
+    hue += (360 / (headingLetters.length / 0.5));
   }
 
   // Set palette colors:
-  for (let i = 0, rgb = 0; i < paletteCells.length; i++) {
+  for (let i = 0, hue = 0, rgb = 0; i < paletteCells.length; i++) {
     if (i < paletteCells.length * 0.8) {
-      const length = (paletteCells.length * 0.8) / 2;
-      paletteCells[i].style.background = rainbow(i, length);
+      paletteCells[i].style.background = makeColor(hue);
+      hue += (360 / (paletteCells.length * 0.8));
     } else {
-      paletteCells[i].style.background = grayscale(rgb);
+      paletteCells[i].style.background = makeGrayscale(rgb);
       rgb += Math.round(255 / 8);
     }
   }
